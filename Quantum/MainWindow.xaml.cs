5e8ec2e77a075a99c4a88607384e600a3dcfc5e4
Григорий Ким <kim-g@yandex.ru>
2017-10-23 13:28:38 +0500

    Всё формирует

--- Quantum/MainWindow.xaml.cs
+++ Quantum/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Quantum
 {
     /// <summary>
@@ -23,6 +25,61 @@ namespace Quantum
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Ищем все темплаты
+            string[] allTemplates = Directory.GetFiles(TemplateDir.Text, "*", SearchOption.TopDirectoryOnly);
+            List<string> Templates = new List<string>();
+            List<string> TemplateNames = new List<string>();
+            foreach (string file in allTemplates)
+            {
+                Templates.Add(File.ReadAllText(file));
+                TemplateNames.Add(System.IO.Path.GetFileName(file));
+            }
+
+            // Получаем значения от и до
+            int From;
+            int To;
+            try
+            { From = Convert.ToInt32(from_text.Text); }
+            catch
+            { MessageBox.Show("Введите число в поле От","Ошибка"); return; }
+            try
+            { To = Convert.ToInt32(to_text.Text); }
+            catch
+            { MessageBox.Show("Введите число в поле До", "Ошибка"); return; }
+
+            // получаем рабочую директорию
+            string WorkDirectory = System.IO.Path.Combine(Dir_text.Text, Project_text.Text);
+
+            // И проходим по всем внутренним директориям
+            for (int i= From; i <= To; i++)
+            {
+                // Формируем название
+                string CurDir = i.ToString("D2");
+                string CurAddress = System.IO.Path.Combine(WorkDirectory, CurDir);
+                string CurDirLocalUnix = System.IO.Path.Combine(Project_text.Text, CurDir).Replace('\\','/');
+
+                // Создаём соответствующую директорию
+                Directory.CreateDirectory(CurAddress);
+
+                // И записывает туда изменённые файлы
+                for (int j=0; j<Templates.Count; j++)
+                {
+                    File.WriteAllText(System.IO.Path.Combine(CurAddress, TemplateNames[j]),
+                        Templates[j].Replace("@Dir@", CurDirLocalUnix).Replace("@N@", CurDir));
+                }
+              
+            }
+
+            MessageBox.Show("Файлы созданы успешно", "Работа завершена");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
