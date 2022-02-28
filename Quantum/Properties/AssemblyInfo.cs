d7d7d1d65fd4cc0c760ce23d84eb1019adb1ac91
Григорий Ким <kim-g@yandex.ru>
2022-02-28 18:13:13 +0500

    Изменения сведений о сборке

--- Quantum/Properties/AssemblyInfo.cs
+++ Quantum/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Quantum")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Программа для создания вводных файлов для программы Orca (квантово-химические расчёты)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ИОС УрО РАН")]
 [assembly: AssemblyProduct("Quantum")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
